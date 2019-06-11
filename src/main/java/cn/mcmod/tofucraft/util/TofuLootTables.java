76fe697db34374bdd8c9f6fb055cd35ac9abadd2
pentantan <shutux500@gmail.com>
2019-06-11 22:28:01 +0900

    add saltpan recipe

--- src/main/java/cn/mcmod/tofucraft/util/TofuLootTables.java
+++ src/main/java/cn/mcmod/tofucraft/util/TofuLootTables.java
@@ -7,6 +7,7 @@ public class TofuLootTables {
     public static final ResourceLocation tofucow = new ResourceLocation(TofuMain.MODID, "entity/tofucow");
     public static final ResourceLocation tofuslime = new ResourceLocation(TofuMain.MODID, "entity/tofu_slime");
     public static final ResourceLocation tofuchinger = new ResourceLocation(TofuMain.MODID, "entity/tofu_chinger");
+    public static final ResourceLocation tofuspider = new ResourceLocation(TofuMain.MODID, "entity/tofuspider");
     public static final ResourceLocation tofuhouse = new ResourceLocation(TofuMain.MODID,"chest/tofuvillage/tofuhouse");
     public static final ResourceLocation tofufish = new ResourceLocation(TofuMain.MODID,"entity/tofufish");
     public static final ResourceLocation tofumineshaft = new ResourceLocation(TofuMain.MODID,"chest/tofumineshaft");
