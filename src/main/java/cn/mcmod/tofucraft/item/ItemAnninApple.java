ac48b3ff2bbd7726d9726d303d63c12c551b09f9
pentantan <shutux500@gmail.com>
2019-07-14 15:57:11 +0900

    add yuba_noren and some tweek

--- src/main/java/cn/mcmod/tofucraft/item/ItemAnninApple.java
+++ src/main/java/cn/mcmod/tofucraft/item/ItemAnninApple.java
@@ -3,7 +3,7 @@ package cn.mcmod.tofucraft.item;
 import cn.mcmod.tofucraft.TofuMain;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.EnumRarity;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -12,19 +12,19 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class ItemAnninApple extends Item {
+public class ItemAnninApple extends ItemFood {
     public ItemAnninApple() {
+        super(5,0.3F,false);
         this.setUnlocalizedName(TofuMain.MODID + "." + "annin_apple");
     }
 
     @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
         tooltip.add("I can't think it looks so delicious ...");
-        tooltip.add("but mayby Tofu creatures are likely to be eaten");
     }
 
     @Override
     public EnumRarity getRarity(ItemStack stack) {
-        return EnumRarity.RARE;
+        return EnumRarity.COMMON;
     }
 }
