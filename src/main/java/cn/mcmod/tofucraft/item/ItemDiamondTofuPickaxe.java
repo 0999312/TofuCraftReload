43e59271c6e6f6390922577dd5702e869e5a1113
pentantan <shutux500@gmail.com>
2019-08-23 14:55:37 +0900

    fixed my mistaje

--- src/main/java/cn/mcmod/tofucraft/item/ItemDiamondTofuPickaxe.java
+++ src/main/java/cn/mcmod/tofucraft/item/ItemDiamondTofuPickaxe.java
@@ -8,18 +8,21 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 
-public class ItemDiamondTofuPickaxe extends ItemPickaxeBasic implements IEnergyExtractable {
-
+public class ItemDiamondTofuPickaxe extends ItemPickaxeBasic implements IEnergyExtractable
+{
     private DiamondTofuToolHandler impl;
 
-    public ItemDiamondTofuPickaxe() {
+    public ItemDiamondTofuPickaxe()
+    {
         super(TofuToolMaterial.DIAMOND, "tooldiamondpickaxe");
         this.impl = new DiamondTofuToolHandler(this);
     }
 
     @Override
-    public boolean onBlockStartBreak(ItemStack stack, BlockPos pos, EntityPlayer owner) {
-        if (owner.world.isRemote) {
+    public boolean onBlockStartBreak(ItemStack stack, BlockPos pos, EntityPlayer owner)
+    {
+        if (owner.world.isRemote)
+        {
             Block blockDestroyed = owner.getEntityWorld().getBlockState(pos).getBlock();
             this.impl.onBlockStartBreak(stack, owner.world, blockDestroyed, pos, owner);
         }
@@ -27,7 +30,8 @@ public class ItemDiamondTofuPickaxe extends ItemPickaxeBasic implements IEnergyE
     }
 
     @Override
-    public int drain(ItemStack inst, int amount, boolean simulate) {
+    public int drain(ItemStack inst, int amount, boolean simulate)
+    {
         if (!simulate) ItemUtils.damageItemStack(inst, 1);
         return 50;
     }
