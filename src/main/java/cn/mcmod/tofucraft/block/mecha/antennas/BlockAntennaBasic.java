cb3e5029fe5173a34893b664a71f2473d91214a6
pentantan <shutux500@gmail.com>
2019-08-28 04:52:34 +0900

    add antenna,battery texture and new tofurain!

--- src/main/java/cn/mcmod/tofucraft/block/mecha/antennas/BlockAntennaBasic.java
+++ src/main/java/cn/mcmod/tofucraft/block/mecha/antennas/BlockAntennaBasic.java
@@ -4,8 +4,15 @@ import cn.mcmod.tofucraft.CommonProxy;
 import cn.mcmod.tofucraft.base.block.IAnntena;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockAntennaBasic extends Block implements IAnntena {
     /*
@@ -24,5 +31,22 @@ public class BlockAntennaBasic extends Block implements IAnntena {
     @Override
     public int getPower(BlockPos pos, World world) {
         return 7;
+    }
+
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return BlockFaceShape.UNDEFINED;
     }
 }
